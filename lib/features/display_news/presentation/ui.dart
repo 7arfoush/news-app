6d1335df5b04501c45e2296fe2eb09b9d1775fc6
7arfoush <7arfoush@outlook.com>
2021-05-26 20:00:09 +0200

    Adding firebase support

--- lib/features/display_news/presentation/ui.dart
+++ lib/features/display_news/presentation/ui.dart
@@ -25,77 +25,84 @@ class _DisplayNewsUIState extends State<DisplayNewsUI> {
               Consumer<DisplayNewsProvider>(
                   builder: (context, provider, child) =>
                       FutureBuilder<NewsModel>(
-                        future: provider.getNews(),
-                        builder: (context, snapshot) {
-                          return ListView.builder(
-                            shrinkWrap: true,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 24, vertical: 24),
-                            itemCount: snapshot.data!.articles.length,
-                            physics: NeverScrollableScrollPhysics(),
-                            itemBuilder: (context, index) {
-                              return Container(
-                                margin: EdgeInsets.symmetric(vertical: 12),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      height: 192,
-                                      decoration: BoxDecoration(
-                                          color: Colors.grey,
-                                          boxShadow: [
-                                            BoxShadow(
-                                              blurRadius: 16,
-                                              color: Colors.black
-                                                  .withOpacity(0.16),
-                                            )
-                                          ],
-                                          borderRadius:
-                                              BorderRadius.circular(16),
-                                          image: DecorationImage(
-                                              fit: BoxFit.cover,
-                                              image: Image.network(snapshot
-                                                      .data!
-                                                      .articles[index]
-                                                      .imageUrl)
-                                                  .image)),
+                          future: provider.getNews(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.done) {
+                              return ListView.builder(
+                                shrinkWrap: true,
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 24, vertical: 24),
+                                itemCount: snapshot.data!.articles.length,
+                                physics: NeverScrollableScrollPhysics(),
+                                itemBuilder: (context, index) {
+                                  return Container(
+                                    margin: EdgeInsets.symmetric(vertical: 12),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          height: 192,
+                                          decoration: BoxDecoration(
+                                              color: Colors.grey,
+                                              boxShadow: [
+                                                BoxShadow(
+                                                  blurRadius: 16,
+                                                  color: Colors.black
+                                                      .withOpacity(0.16),
+                                                )
+                                              ],
+                                              borderRadius:
+                                                  BorderRadius.circular(16),
+                                              image: DecorationImage(
+                                                  fit: BoxFit.cover,
+                                                  image: Image.network(snapshot
+                                                          .data!
+                                                          .articles[index]
+                                                          .imageUrl)
+                                                      .image)),
+                                        ),
+                                        SizedBox(
+                                          height: 16,
+                                        ),
+                                        Text(
+                                          snapshot.data!.articles[index]
+                                              .datePublished
+                                              .toIso8601String(),
+                                          style: TextStyle(color: Colors.grey),
+                                        ),
+                                        SizedBox(
+                                          height: 8,
+                                        ),
+                                        Text(
+                                          snapshot.data!.articles[index].title,
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w600),
+                                        ),
+                                        SizedBox(
+                                          height: 16,
+                                        ),
+                                        Text(
+                                          snapshot.data!.articles[index]
+                                              .description,
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                              fontSize: 16, color: Colors.grey),
+                                        ),
+                                      ],
                                     ),
-                                    SizedBox(
-                                      height: 16,
-                                    ),
-                                    Text(
-                                      snapshot
-                                          .data!.articles[index].datePublished
-                                          .toIso8601String(),
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                    SizedBox(
-                                      height: 8,
-                                    ),
-                                    Text(
-                                      snapshot.data!.articles[index].title,
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                    SizedBox(
-                                      height: 16,
-                                    ),
-                                    Text(
-                                      snapshot
-                                          .data!.articles[index].description,
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          fontSize: 16, color: Colors.grey),
-                                    ),
-                                  ],
-                                ),
+                                  );
+                                },
                               );
-                            },
-                          );
-                        },
-                      )),
+                            } else {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                          })),
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/features/display_news/presentation/provider.dart';
 import 'package:news_app/features/display_news/presentation/ui.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  late final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          appBarTheme: AppBarTheme(elevation: 0),
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.green,
-        ),
-        home: ChangeNotifierProvider(
-          create: (context) => DisplayNewsProvider(),
-          builder: (context, child) => DisplayNewsUI(),
-        ));
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+                title: 'Flutter Demo',
+                theme: ThemeData(
+                  appBarTheme: AppBarTheme(elevation: 0),
+                  primarySwatch: Colors.green,
+                ),
+                home: ChangeNotifierProvider(
+                  create: (context) => DisplayNewsProvider(),
+                  builder: (context, child) => DisplayNewsUI(),
+                ));
+          }
+          return MaterialApp(
+            theme: ThemeData(
+              primarySwatch: Colors.green,
+            ),
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        });
   }
 }
 
